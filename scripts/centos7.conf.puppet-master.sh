d9778992d3871a48bc467a18b5b18677b1a196bd
Peterson W. Santos <petersonwsantos@users.noreply.github.com>
2016-05-28 21:27:01 -0300

    Update centos7.conf.puppet-master.sh

--- scripts/centos7.conf.puppet-master.sh
+++ scripts/centos7.conf.puppet-master.sh
@@ -3,3 +3,9 @@ rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum update
 yum install puppetserver -y
  
+echo "[agent]" >>  /etc/puppetlabs/puppet/puppet.conf
+echo "certname = puppetserver.virti.local" >>  /etc/puppetlabs/puppet/puppet.conf
+
+[agent]
+
+certname = puppetserver.virti.local
