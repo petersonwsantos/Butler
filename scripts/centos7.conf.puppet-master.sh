5a7f90b0e9067fefaf6fdce2f65adf753c567bd2
Peterson W. Santos <petersonwsantos@users.noreply.github.com>
2016-05-28 21:42:16 -0300

    Update centos7.conf.puppet-master.sh

--- scripts/centos7.conf.puppet-master.sh
+++ scripts/centos7.conf.puppet-master.sh
@@ -2,10 +2,24 @@
 rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum update
 yum install puppetserver -y
- 
+
+# limpa entradas anteriores
+sed -i "s/[agent/#[agent/"  /etc/puppetlabs/puppet/puppet.conf
+sed -i "s/certname/#certname/"  /etc/puppetlabs/puppet/puppet.conf
+
+
 echo "[agent]" >>  /etc/puppetlabs/puppet/puppet.conf
-echo "certname = puppetserver.virti.local" >>  /etc/puppetlabs/puppet/puppet.conf
+echo "certname = `hostname -f`" >>  /etc/puppetlabs/puppet/puppet.conf
 
-[agent]
+#   parâmetros de memória da JVM,
+echo "START_TIMEOUT=400" >> /etc/sysconfig/puppetserver
 
-certname = puppetserver.virti.local
+
+echo "Gerar certificados"
+echo " "
+echo " "
+echo "Digite:  "
+echo "puppet cert list -a"
+echo "puppet cert generate  --dns_alt_names=puppet"
+echo " "
+echo "systemctl restart puppetserver"
