43a653f0d4a399fd20df41133760e3a7665ead46
Peterson W. Santos <petersonwsantos@users.noreply.github.com>
2017-06-28 18:57:12 -0300

    Update centos7.conf.puppet-agent.sh

--- scripts/centos7.conf.puppet-agent.sh
+++ scripts/centos7.conf.puppet-agent.sh
@@ -1,12 +1,6 @@
 #! /bin/bash
-yum remove puppet-agent  -y
-yum remove puppetlabs-release-pc1  -y
-rm -rfv /opt/puppetlabs
-rm -rfv /etc/puppetlabs
-rpm -qa | grep puppet
-
-wget https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
-rpm -hiv puppetlabs-release-pc1-el-7.noarch.rpm
+yum install wget -y 
+rpm -Uvh  https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 rm -fv puppetlabs-release-pc1*
 yum install puppet-agent -y 
 echo 'PATH=$PATH:/opt/puppetlabs/puppet/bin' > /etc/profile.d/append-puppetlabs-path.sh
